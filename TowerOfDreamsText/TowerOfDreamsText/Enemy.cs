1702a7c6a5988ebfafe95adc2d21b67c7e1b6324
AlexParrotto <aap5033@rit.edu>
2024-09-30 11:50:24 -0400

    Added basic gameplay

---
+++ TowerOfDreamsText/TowerOfDreamsText/Enemy.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerOfDreamsText
+{
+    internal class Enemy
+    {
+        private int health;
+        public int Health 
+        {
+            get 
+            {
+                return health;
+            }
+            set 
+            { 
+                health = value; 
+            }
+        }
+        private int damage;
+        private bool elite;
+        private int eliteRand;
+        private int nameRand;
+        private string name;
+        public Enemy() 
+        {
+            Random rand = new Random();
+            health = rand.Next(2, 4);
+            damage = rand.Next(1, 2);
+            nameRand = rand.Next(1, 3);
+            switch(nameRand)
+            {
+                case 1:
+                    name = "Slime";
+                    break;
+                case 2:
+                    name = "Pop Cannon";
+                    break;
+                case 3:
+                    name = "Gravitro";
+                    break;
+            }
+            eliteRand = rand.Next(1, 5);
+            if (eliteRand == 2)
+            {
+                elite = true;
+                name = "Elite " + name;
+                damage *= 2;
+                health *= 2;
+            }
+            else elite = false;
+            // Print details on enemy creation
+            Console.WriteLine("You encounter a " + name + "!");
+            Console.WriteLine(name + " has " + health + " health");
+            Console.WriteLine();
+        }
+
+        public int Attack()
+        {
+            Console.WriteLine(name + " attacks for " + damage + " damage!");
+            return damage;
+        }
+
+        public void TakeDamage(int playerDamage)
+        {
+            Console.WriteLine(name + " takes " + damage + " damage!");
+            health -= playerDamage;
+            Console.WriteLine(name + " has " + health + " health remaining!");
+        }
+
+        public bool Die()
+        {
+            if(health <= 0)
+            {
+                Console.WriteLine(name + " has perished!");
+                Console.WriteLine();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ TowerOfDreamsText/TowerOfDreamsText/MainLoop.cs
@@ -0,0 +1,33 @@
+﻿using TowerOfDreamsText;
+
+string input = "";
+int health = 6;
+int attack = 2;
+int gems = 0;
+Enemy currentEnemy = new Enemy();
+int enemiesRemaining = 5;
+
+while(input != "q")
+{
+    Console.WriteLine("What would you like to do?");
+    Console.WriteLine("a: Attack");
+    Console.WriteLine("b: Block");
+    Console.WriteLine("j: Jump");
+    Console.WriteLine("q: Quit");
+    input = Console.ReadLine();
+    switch(input)
+    {
+        case "a":
+            Console.WriteLine("You attack!");
+            currentEnemy.TakeDamage(attack);
+            if (currentEnemy.Die()) currentEnemy = new Enemy();
+            break;
+        case "b":
+            Console.WriteLine("You block!");
+            break;
+        case "j":
+            Console.WriteLine("You jump over the enemy!");
+            break;
+    }
+    Console.WriteLine();
+}
